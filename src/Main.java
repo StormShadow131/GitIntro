d5151906d0aa67c091c5e11265bec916e787d8b0
Shado <anais.q33@gmail.com>
2024-10-07 13:51:25 +0200

    Modif master

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
         System.out.println("Modif git");
+        System.out.println("Modif Master");
     }
 }
